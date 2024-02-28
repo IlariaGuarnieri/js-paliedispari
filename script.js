b6f1eaf2370aec15501af6357e0d4abb8ee37875
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-02-28 15:26:55 +0100

    [add] inizio javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+const pariDispariUtente = prompt('scegli se pari o dispari')
+console.log(pariDispariUtente)
+
+const numeroUtente = prompt('scegli un numero da 1 a 5')
+console.log(numeroUtente)
+
+
+
+
+
+
+
+
+
+
+
+
+
+function numeriRandom (){
+
+}
